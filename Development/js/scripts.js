c6cdb5506e55490e2ad5c42d161ee8577415a809
husienalnaser <husien_alnaser@hotmail.com>
2020-06-27 20:18:55 +0300

    cw done

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -1,29 +1,34 @@
 // السؤال الأول
 // 1
-
+const name='husien';
 // 2
-
+let age='16';
 // 3 and 4
-
+console.log('my name is'+' '+name+' '+'and my age is'+' '+age);
 // 5 and 6
-
+console.log(age*5);
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // السؤال الثاني
 // 1
-
+let hobbies=['swimming','football','shooting']; 
 // 2
-
+hobbies.push(age);
 // 3
-
+hobbies.pop ;
+console.log(hobbies);
 // 4
-
+let husien={
+    track:'web',
+    languge:'css',
+    TAs:['moudhi','husien','nancy','rahaf'],
+};
 // 5
 
 // 6
-
+console.log(husien.track);
 // 7
-
+console.log(husien.TAs[0]);
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // البونص
